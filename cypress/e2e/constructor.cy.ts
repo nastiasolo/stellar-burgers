83c2d41aea3ca8d19bd05ead3569c9bc27038f67
nastiasolo <ag.solodova@gmail.com>
2024-11-18 15:49:18 +0100

    cypress tests added

---
+++ cypress/e2e/constructor.cy.ts
@@ -0,0 +1,104 @@
+/// <reference types="cypress" />
+describe('add ingredients works correctly', function () {
+  beforeEach(function () {
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+    cy.viewport(1300, 800);
+    cy.visit('http://localhost:4000');
+  });
+
+  it('should add bun', function () {
+    cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
+    cy.get('[data-cy=constructor-bun-1]')
+      .contains('Ингредиент 1')
+      .should('exist');
+    cy.get('[data-cy=constructor-bun-2]')
+      .contains('Ингредиент 1')
+      .should('exist');
+  });
+
+  it('should add ingredient', function () {
+    cy.get('[data-cy=mains-ingredients]').contains('Добавить').click();
+    cy.get('[data-cy=sauces-ingredients]').contains('Добавить').click();
+    cy.get('[data-cy=constructor-ingredients]')
+      .contains('Ингредиент 2')
+      .should('exist');
+    cy.get('[data-cy=constructor-ingredients]')
+      .contains('Ингредиент 4')
+      .should('exist');
+  });
+});
+
+describe('ingredients modal works correctly', function () {
+  beforeEach(function () {
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+    cy.viewport(1300, 800);
+    cy.visit('http://localhost:4000');
+  });
+
+  it('should work open modal', function () {
+    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains('Ингредиент 1').click();
+    cy.contains('Детали ингредиента').should('exist');
+    cy.get('#modals').contains('Ингредиент 1').should('exist');
+  });
+
+  it('should work close modal on button click', function () {
+    cy.contains('Ингредиент 1').click();
+    cy.contains('Детали ингредиента').should('exist');
+    cy.get('#modals button[aria-label="Закрыть"]').click();
+    cy.contains('Детали ингредиента').should('not.exist');
+  });
+
+  it('should work close modal on overlay click', function () {
+    cy.contains('Ингредиент 1').click();
+    cy.contains('Детали ингредиента').should('exist');
+    cy.get('[data-cy=modal-overlay]').click('left', { force: true });
+    cy.contains('Детали ингредиента').should('not.exist');
+  });
+});
+
+describe('order modal works correctrly', function () {
+  beforeEach(function () {
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
+      'ingredients'
+    );
+    cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
+
+    cy.intercept('POST', 'api/orders', { fixture: 'order.json' }).as(
+      'postOrder'
+    );
+
+    window.localStorage.setItem(
+      'refreshToken',
+      JSON.stringify('test-refreshToken')
+    );
+    cy.setCookie('accessToken', 'test-accessToken');
+    cy.viewport(1300, 800);
+    cy.visit('http://localhost:4000');
+  });
+
+  afterEach(function () {
+    cy.clearAllLocalStorage();
+    cy.clearCookies();
+  });
+
+  it('should order burger work', function () {
+    cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
+    cy.get('[data-cy=mains-ingredients]').contains('Добавить').click();
+    cy.get('[data-cy=sauces-ingredients]').contains('Добавить').click();
+    cy.get('[data-cy=order-summ] button').click();
+
+    cy.wait('@postOrder')
+      .its('request.body')
+      .should('deep.equal', {
+        ingredients: ['1', '2', '4', '1']
+      });
+
+    cy.get('[data-cy=order-number]').contains('123456').should('exist');
+
+    cy.get('#modals button[aria-label="Закрыть"]').click();
+    cy.get('[data-cy=order-number]').should('not.exist');
+
+    cy.get('[data-cy=bun-ingredients]').should('not.exist');
+  });
+});
